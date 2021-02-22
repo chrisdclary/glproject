095484a91ce947f2c1bb9e520584a0a6e70ed984
Chris Clary <chrisdennisclary@gmail.com>
2021-02-21 18:31:16 -0800

    Adding compiled exe and object files because why not

---
+++ obj/engine/glad/glad.c.d
@@ -0,0 +1,2 @@
+obj/engine/glad/glad.c.o: src/engine/glad/glad.c src/engine/glad/glad.h \
+ src/engine/glad/khrplatform.h
---
+++ obj/main.cpp.d
@@ -0,0 +1,1 @@
+obj/main.cpp.o: src/main.cpp src/game.h src/engine/engine.h
